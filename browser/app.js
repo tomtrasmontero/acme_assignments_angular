6b6f105d49bb3deb2742dff8ed27cc249d7b8f98
tomtrasmontero <tomtrasmontero@gmail.com>
2016-09-27 02:41:27 -0400

    updated states resolve

--- browser/app.js
+++ browser/app.js
@@ -5,17 +5,31 @@ angular.module('salesAngular')
 		$stateProvider
 			.state('home', {
 				url: '/',
-				templateUrl: '/home.html'
+				templateUrl: '/home.html',
+				resolve: {
+					all: function(SalesPeopleFactory,RegionsFactory,$q){
+						return $q.all([SalesPeopleFactory.findAll(), RegionsFactory.findAll()]);				
+					}			
+				}				
 			})
 			.state('regions', {
 				url: '/regions',
 				templateUrl: '/regions/regions.html',
+				resolve: {
+					all: function(SalesPeopleFactory,RegionsFactory,$q){
+						return $q.all([SalesPeopleFactory.findAll(), RegionsFactory.findAll()]);				
+					}				
+				}					
 			})
 			.state('salesPeople', {
 				url: '/salesPeople',
-				templateUrl: '/salesPeople/salesPeople.html'
+				templateUrl: '/salesPeople/salesPeople.html',
+				resolve: {
+					all: function(SalesPeopleFactory,RegionsFactory,$q){
+						return $q.all([SalesPeopleFactory.findAll(), RegionsFactory.findAll()]);				
+					}				
+				}				
 			})
-
 			$urlRouterProvider.otherwise('/');
 	});
 
